5ee28605188f0f87c0129691f704cad542f43ec1
Javier <javiermacasaet@gmail.com>
2026-02-19 09:11:12 +0800

    Implement recipe list and detail pages

--- ledger/views.py
+++ ledger/views.py
@@ -1,45 +1,14 @@
 from django.shortcuts import render
+from .models import Recipe
 
-# Create your views here.
+
 def recipe_list(request):
-    ctx = {
-        "recipes": [
-            {
-                "name": "Recipe 1",
-                "link": "/recipe/1",
-            },
-            {
-                "name": "Recipe 2",
-                "link": "/recipe/2",
-            }
-        ]
-    }
-    return render(request, "ledger/recipe_list.html", ctx)
+    recipes = Recipe.objects.all()
+    ctx = {"recipes": recipes}
+    return render(request, "recipe_list.html", ctx)
 
-def recipe_1(request):
-    ctx = {
-        "name": "Recipe 1",
-        "ingredients": [
-            {"name": "tomato", "quantity": "3pcs"},
-            {"name": "onion", "quantity": "1pc"},
-            {"name": "pork", "quantity": "1kg"},
-            {"name": "water", "quantity": "1L"},
-            {"name": "sinigang mix", "quantity": "1 packet"},
-        ]
-    }
-    return render(request, "ledger/recipe_detail.html", ctx)
 
-def recipe_2(request):
-    ctx = {
-        "name": "Recipe 2",
-        "ingredients": [
-            {"name": "garlic", "quantity": "1 head"},
-            {"name": "onion", "quantity": "1pc"},
-            {"name": "vinegar", "quantity": "1/2cup"},
-            {"name": "water", "quantity": "1 cup"},
-            {"name": "salt", "quantity": "1 tablespoon"},
-            {"name": "whole black peppers", "quantity": "1 tablespoon"},
-            {"name": "pork", "quantity": "1 kilo"},
-        ]
-    }
-    return render(request, "ledger/recipe_detail.html", ctx)
+def recipe_detail(request, id):
+    recipe = Recipe.objects.get(pk=id)
+    ctx = {"recipe": recipe}
+    return render(request, "recipe_detail.html", ctx)
