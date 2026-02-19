43873c99a247d5fa2d434414df304f027de19c93
Javier <javiermacasaet@gmail.com>
2026-02-19 09:29:31 +0800

    Added ledger urls

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import recipe_list, recipe_1, recipe_2
 
-app_name = "ledger"
+from .views import recipe_detail, recipe_list
+
+app_name = 'ledger'
 
 urlpatterns = [
-    path("recipes/list", recipe_list, name="recipe-list"),
-    path("recipe/1", recipe_1, name="recipe-1"),
-    path("recipe/2", recipe_2, name="recipe-2"),
+    path('', recipe_list, name='list'),
+    path('<int:id>/', recipe_detail, name='recipe-detail'),
 ]
