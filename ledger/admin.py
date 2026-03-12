af4f731427566cc01418ec8c60acb87a1370296e
Javier <javiermacasaet@gmail.com>
2026-03-12 10:55:51 +0800

    Add RecipeImage inline to Django admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-from .models import Ingredient, Recipe, RecipeIngredient, Profile
+from .models import Ingredient, Recipe, RecipeIngredient, Profile, RecipeImage
 
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
 
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
+
+
 class RecipeAdmin(admin.ModelAdmin):
-    inlines = [RecipeIngredientInline]
+    inlines = [RecipeIngredientInline, RecipeImageInline]
 
 
 admin.site.register(Ingredient)
