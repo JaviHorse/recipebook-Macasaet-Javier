fcc5f59b86d10e7dd2120bdf3989524a40efa3ed
Javier <javiermacasaet@gmail.com>
2026-03-12 10:55:18 +0800

    Fix recipe URLs and configure template/media settings

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import recipe_detail, recipe_list
+from .views import recipe_detail, recipe_list, recipe_add, add_image
 
 app_name = 'ledger'
 
 urlpatterns = [
-    path('', recipe_list, name='list'),
-    path('<int:id>/', recipe_detail, name='recipe-detail'),
+    path('recipe/', recipe_list, name='recipe-list'),
+    path('recipe/add/', recipe_add, name='recipe-add'),
+    path('recipe/<int:id>/', recipe_detail, name='recipe-detail'),
+    path('recipe/<int:id>/add_image/', add_image, name='add-image'),
 ]
